befcc8e2762586880918f42c7efaf056385166ff
niwayama <niwayama.takeyuki@b-architects.com>
2021-01-30 21:35:00 +0900

    create form and validate with error messages

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,55 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
+class StreamCreate extends React.Component{
+
+    renderInput(formProps){
+        console.log(formProps.meta)
+        return (
+            <div className="field">
+                <label>{formProps.label}</label>
+                <input {...formProps.input} />
+                <div>{formProps.meta.error}</div>
+            </div>
+        )
+    }
+
+    onSubmit(formValue){
+        console.log(formValue)
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)}
+                className="ui form"
+            >
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field
+                    name="description"
+                    component={this.renderInput}
+                    label="Enter Description"
+                />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate
+const validate = (formValues) => {
+    const errors = {}
+
+    if(!formValues.title){
+        errors.title = 'You must enter a title'
+    }
+
+    if(!formValues.description){
+        errors.description = 'You must enter a description'
+    }
+
+    return errors
+}
+
+export default reduxForm({
+    form:'streamCreate',
+    validate
+})(StreamCreate)
