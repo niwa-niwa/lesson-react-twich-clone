8f3599526dc3991f740f738509c1ef80e7877d55
niwayama <niwayama.takeyuki@b-architects.com>
2021-01-30 16:14:42 +0900

    implement Google sign on

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import GoogleAuth from './GoogleAuth'
 
 const Header = () => {
     return (
@@ -13,8 +14,9 @@ const Header = () => {
                 <Link to="/" className="item">
                     All Streams
                 </Link>
+                <GoogleAuth />
             </div>
-            
+
         </div>
     )
 }
