a396cfac1dd49832c75ac43b52b321c31d65ecae
niwayama <niwayama.takeyuki@b-architects.com>
2021-01-30 12:25:53 +0900

    implemented router and link

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import { BrowserRouter, Route, Link} from 'react-router-dom'
+
+const PageOne = () => {
+    return (
+        <div>
+            PageOne
+            <Link to="/pagetwo">Navigate to page two</Link>
+        </div>
+    )
+}
+
+const PageTwo = () => {
+    return (
+        <div>
+            PageTwo
+            <Link to="/">Navigate to page one</Link>
+        </div>
+    )
+}
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
