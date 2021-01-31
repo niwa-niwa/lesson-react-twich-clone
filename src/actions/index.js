14d095b9863bc539e7c02a311011cd226d61fe2b
niwayama <niwayama.takeyuki@b-architects.com>
2021-01-31 16:02:01 +0900

    fixed rendering edit-page  after reload

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,16 +36,16 @@ export const fetchStreams = () => async (dispatch) => {
 }
 
 export const fetchStream = (id) => async (dispatch) => {
-    const response  = await streams.get(`/stream/${id}`)
+    const response  = await streams.get(`/streams/${id}`)
     dispatch({type:FETCH_STREAM, payload:response.data})
 }
 
 export const editStream = (id, formValues) => async (dispatch) => {
-    const response = await streams.put(`/streams/edit/${id}`, formValues)
+    const response = await streams.put(`/streams/${id}`, formValues)
     dispatch({type:EDIT_STREAM, payload:response.data})
 }
 
 export const deleteStream = (id) => async (dispatch) => {
-    await streams.delete(`/streams/delete/${id}`)
+    await streams.delete(`/streams/${id}`)
     dispatch({type:DELETE_STREAM, payload: id })
 }
