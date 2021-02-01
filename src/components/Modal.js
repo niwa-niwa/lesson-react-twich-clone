c0043aa0f6f5b710a956bd27113076bf6c1d1cae
niwayama <niwayama.takeyuki@b-architects.com>
2021-02-01 23:25:13 +0900

    added React.Fragment

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,16 +9,15 @@ const Modal = props => {
             className="ui dimmer modals visible active"
         >
             <div
-                // onClick={(e) => e.stopPropagation()}
+                onClick={(e) => e.stopPropagation()} // HTMLの仕様上親のイベントが優先されるのを防ぐ
                 className="ui standard modal visible active"
             >
-                <div className="header">Delete Stream</div>
+                <div className="header">{props.title}</div>
                 <div className="content">
-                    Are you sure you want to delete this stream?
+                    {props.content}
                 </div>
                 <div className="actions">
-                    <button className="ui primary button">Delete</button>
-                    <button className="ui button">Cancel</button>
+                    {props.actions}
                 </div>
             </div>
         </div>,
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,24 @@ import React from 'react'
 import Modal from '../Modal'
 
 const StreamDelete = () => {
+
+    const actions = (
+        // Fragment tag is just binding elements the tag is not appeared in HTML source
+        <React.Fragment>
+            <div>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </div>
+        </React.Fragment>
+    )
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal
+                title='Delete Stream'
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+            />
         </div>
     )
 }
